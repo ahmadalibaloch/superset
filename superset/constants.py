d45a9a54b40e89a62594941e9c97f894acbaa365
Ahmad Ali <ahmadalibaloch@gmail.com>
2025-01-26 02:12:39 +0100

    Update constants.py strong secret key

--- superset/constants.py
+++ superset/constants.py
@@ -27,7 +27,7 @@ USER_AGENT = "Apache Superset"
 NULL_STRING = "<NULL>"
 EMPTY_STRING = "<empty string>"
 
-CHANGE_ME_SECRET_KEY = "l9RvWfYvZ313z9zM1qdy4ha1Cg6GREePW4iTzcL0atxx7cV8mMisQbpfvELxDkbUFFKuBEfW8McGBoPKXbWf_g"  # noqa: S105
+CHANGE_ME_SECRET_KEY = "61cSJ9KpofL9W/6G6nidW/KiMVvRaR754Ai4sPYrMlH5nooEX5XEB5lp"  # noqa: S105
 
 # UUID for the examples database
 EXAMPLES_DB_UUID = "a2dc77af-e654-49bb-b321-40f6b559a1ee"
